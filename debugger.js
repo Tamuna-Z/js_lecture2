3b3da41576f8f2cf87e429c2e60027e6028c2a47
Tamuna-Z <tamunazurabashvili3@gmail.com>
2025-03-21 21:12:52 +0400

    add last lecture

---
+++ debugger.js
@@ -0,0 +1,16 @@
+debugger
+
+let username = 'tamuna'
+let age = 30
+let info = username + ' ' +age
+console.log(info);
+let city = ['tbilis', 'gori']
+function meet(){
+    
+    for(let i =0; i<city.length; i++ ){
+        console.log("hello");
+    }
+
+}
+
+meet()
